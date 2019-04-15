8dc1ad07e96a40aaabf89f3ad3b94d717a2d75d8
xujiangshui <2386686364@qq.com>
2019-04-15 23:49:37 +0800

    静态资源文件访问

---
+++ springboot/src/main/java/com/shui/springbootvue/config/MyWebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.shui.springbootvue.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 配置静态资源访问
+ */
+//@Configuration
+//public class MyWebMvcConfig implements WebMvcConfigurer{
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry
+//                .addResourceHandler("/static/**")
+//                .addResourceLocations("classpath:/static/");
+//    }
+//}

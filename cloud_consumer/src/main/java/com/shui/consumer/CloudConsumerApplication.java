47b2d5d1aa814db9f32059d4cf699c5f77665b97
shuiing <2386686364@qq.com>
2019-09-12 11:13:50 +0800

    【cloud_consumer】 消费者

---
+++ cloud_consumer/src/main/java/com/shui/consumer/CloudConsumerApplication.java
@@ -0,0 +1,25 @@
+package com.shui.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class CloudConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConsumerApplication.class, args);
+    }
+
+    /**
+     * 用于请求接口
+     * 原理：封装了httpclient
+     */
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}

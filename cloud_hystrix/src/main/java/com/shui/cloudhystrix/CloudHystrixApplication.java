fcacf3415c4882d6e5dc4b9714ef08d768285dcf
shui <2386686364@qq.com>
2019-10-11 15:54:11 +0800

    【cloud-hytrix】 监控面板

--- cloud_hystrix/src/main/java/com/shui/cloudhystrix/CloudHystrixApplication.java
+++ cloud_hystrix/src/main/java/com/shui/cloudhystrix/CloudHystrixApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @EnableCircuitBreaker
 @SpringBootApplication
+@EnableHystrixDashboard
 public class CloudHystrixApplication {
 
     public static void main(String[] args) {

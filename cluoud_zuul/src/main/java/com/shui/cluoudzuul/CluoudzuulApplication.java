88ac54166bce4e43dfd38a9a2a5fe739888f04c5
shui <2386686364@qq.com>
2019-10-12 10:48:10 +0800

    【cloud-zuul】 聚合自动生成api

--- cluoud_zuul/src/main/java/com/shui/cluoudzuul/CluoudzuulApplication.java
+++ cluoud_zuul/src/main/java/com/shui/cluoudzuul/CluoudzuulApplication.java
@@ -1,5 +1,6 @@
 package com.shui.cluoudzuul;
 
+import com.didispace.swagger.butler.EnableSwaggerButler;
 import com.shui.cluoudzuul.config.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 @EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableSwaggerButler
 public class CluoudzuulApplication {
 
     public static void main(String[] args) {

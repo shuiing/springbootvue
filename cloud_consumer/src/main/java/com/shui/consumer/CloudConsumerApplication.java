5e8cb4288e9c64e61fbae26ddce7053211e291ac
shui <2386686364@qq.com>
2019-10-12 10:35:05 +0800

    【cloud-consumer】 自动生成api

--- cloud_consumer/src/main/java/com/shui/consumer/CloudConsumerApplication.java
+++ cloud_consumer/src/main/java/com/shui/consumer/CloudConsumerApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @EnableDiscoveryClient
 @SpringBootApplication
 public class CloudConsumerApplication {
--- cloud_consumer/src/main/java/com/shui/consumer/controller/Democontroller.java
+++ cloud_consumer/src/main/java/com/shui/consumer/controller/Democontroller.java
@@ -1,9 +1,13 @@
 package com.shui.consumer.controller;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -25,6 +29,20 @@ public class Democontroller {
         String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/dc";
         System.out.println(url);
         return restTemplate.getForObject(url, String.class);
+    }
+
+
+    /**
+     * 自动生成api
+     */
+    @ApiOperation(value = "计算+", notes = "加法")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "a", paramType = "path", value = "数字a", required = true, dataType = "Long"),
+            @ApiImplicitParam(name = "b", paramType = "path", value = "数字b", required = true, dataType = "Long")
+    })
+    @GetMapping("/{a}/{b}")
+    public Integer get(@PathVariable Integer a, @PathVariable Integer b) {
+        return a + b;
     }
 
 }

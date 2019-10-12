3ee765d2b8b4155d91006200609bd49862ec927c
shui <2386686364@qq.com>
2019-10-12 13:42:51 +0800

    【cloud-rabbit】 消费者

---
+++ cloud_rabbit/src/main/java/com/shui/cloudrabbit/CloudrabbitApplication.java
@@ -0,0 +1,13 @@
+package com.shui.cloudrabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudrabbitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudrabbitApplication.class, args);
+    }
+
+}

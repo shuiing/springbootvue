892e5a8e0feeb9306637ca1090103a742e94dc88
shui <2386686364@qq.com>
2019-10-18 14:44:17 +0800

    【cloud-rabbit】 消息中间件

---
+++ cloud_rabbitclent/src/main/java/com/shui/rabbit_client/RabbitclientApplication.java
@@ -0,0 +1,13 @@
+package com.shui.rabbit_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitclientApplication.class, args);
+    }
+
+}
